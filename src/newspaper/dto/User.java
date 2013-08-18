af45f1f0eb0baaf92cc94b0c35c7163ea79e12a7
hoangkianh <hoangkianh31592@gmail.com>
2013-08-18 14:41:42 +0700

    Revert "A"
    This reverts commit 473f2d30a28f95faef181cb4e45289eeea2c4854.

--- src/newspaper/dto/User.java
+++
@@ -1,9 +0,0 @@
-package newspaper.dto;
-
-/**
- * @since Aug - 18 - 2013
- * @author Nguyen Hoang Anh
- */
-public class User {
-    
-}
--- src/newspaper/util/MyConstant.java
+++
@@ -1,8 +0,0 @@
-package newspaper.util;
-
-/**
- * @since Aug - 17 - 2013
- * @author Nguyen Hoang Anh
- */
-public final class MyConstant {
-}
