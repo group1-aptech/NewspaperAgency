541c681e7055ee3f0c068a2e9895b8a66bf9477b
hoangkianh <HKA@HKA-Laptop>
2013-08-18 21:47:51 +0700

    User

--- src/newspaper/dto/User.java
+++ src/newspaper/dto/User.java
@@ -5,5 +5,41 @@ package newspaper.dto;
  * @author Nguyen Hoang Anh
  */
 public class User {
-    
+    private int id;
+    private String userName;
+    private String passWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    /**
+     * Create default values for new instance
+     */
+    public User() {
+        id = 0;
+        userName = "";
+        passWord = "";
+    }
+
+    public User(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
 }
