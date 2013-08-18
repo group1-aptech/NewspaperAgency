734db56b93d37bba7c2a7bc5df2cb5c47e3bb0cb
hoangkianh <hoangkianh31592@gmail.com>
2013-08-19 00:32:54 +0700

    19-8-2013 00:32

--- src/newspaper/dto/Bill.java
+++ src/newspaper/dto/Bill.java
@@ -1,9 +1,113 @@
 package newspaper.dto;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @since Aug - 18 - 2013
  * @author Nguyen Hoang Anh
  */
 public class Bill {
-    
+    private int billNo;
+    private int customerID;
+    private String message;
+    private Date prepareDate;
+    private double totalAmout;
+    private int receiverID;
+    private List<BillDetails> details;
+    private double paid;
+
+    public int getBillNo() {
+        return billNo;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getPrepareDate() {
+        return prepareDate;
+    }
+
+    public void setPrepareDate(Date prepareDate) {
+        this.prepareDate = prepareDate;
+    }
+
+    public int getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(int receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    public double getTotalAmout() {
+        return totalAmout;
+    }
+
+    public void setTotalAmout(double totalAmout) {
+        this.totalAmout = totalAmout;
+    }
+
+    public List<BillDetails> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<BillDetails> details) {
+        this.details = details;
+    }
+
+    public double getPaid() {
+        return paid;
+    }
+
+    public void setPaid(double paid) {
+        this.paid = paid;
+    }
+
+    /**
+     * Create default values for new instance
+     */
+    public Bill() {
+        billNo = 0;
+        customerID = 0;
+        message = "";
+        prepareDate = new Date();
+        totalAmout = 0;
+        receiverID  = 0;
+        details = new ArrayList<>();
+    }
+
+    /**
+     * Create values for new instance
+     * @param customerID
+     * @param message
+     * @param prepareDate
+     * @param totalAmout
+     * @param receiverID
+     * @param details
+     * @param paid 
+     */
+    public Bill(int customerID, String message, Date prepareDate, double totalAmout, int receiverID, List<BillDetails> details, double paid) {
+        this.customerID = customerID;
+        this.message = message;
+        this.prepareDate = prepareDate;
+        this.totalAmout = totalAmout;
+        this.receiverID = receiverID;
+        this.details = details;
+        this.paid = paid;
+    }
 }
--- src/newspaper/dto/BillDetails.java
+++ src/newspaper/dto/BillDetails.java
@@ -5,5 +5,52 @@ package newspaper.dto;
  * @author Nguyen Hoang Anh
  */
 public class BillDetails {
-        
+    private int billNo;
+    private int productID;
+    private int quantity;
+
+    public int getBillNo() {
+        return billNo;
+    }
+
+    public void setBillNo(int billNo) {
+        this.billNo = billNo;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * Create default values for new instance
+     */
+    public BillDetails() {
+        billNo = 0;
+        productID = 0;
+        quantity = 0;
+    }
+    
+    /**
+     * Create values for new instance
+     * @param billNo
+     * @param productID
+     * @param quantity 
+     */
+    public BillDetails(int billNo, int productID, int quantity) {
+        this.billNo = billNo;
+        this.productID = productID;
+        this.quantity = quantity;
+    }
 }
